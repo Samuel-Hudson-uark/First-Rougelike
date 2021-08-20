52c088301f216b3d31c197921ee9e6844447349d
Samuel-Hudson-uark <sahudson@email.uark.edu>
2021-08-20 13:35:00 -0500

    Added Monster card prefab

---
+++ Assets/Scripts/MonsterCard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Card")]
+public class MonsterCard : ScriptableObject
+{
+    public new string name;
+    public string text;
+
+    public Sprite artwork;
+
+    public int cost;
+    public int move;
+    public int attack;
+    public int health;
+
+    public void Print()
+    {
+        Debug.Log(name);
+    }
+}
